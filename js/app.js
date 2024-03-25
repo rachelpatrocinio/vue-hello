0045566b5cb1f0540f943bd5b65ad47d2ddd65be
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-03-25 15:38:05 +0100

    Added: printed on screen with a function

--- js/app.js
+++ js/app.js
@@ -5,6 +5,16 @@ createApp({
         return {
             titolo: "Il mio primo esercizio Vue",
             data: '25/03/2024',
+            //inputValue: 'ciao', // qui ho inserito il value nell'input
+            inputValue: '',
+            printedValue: ''
+        }
+    },
+    methods: {
+        printValue(){
+            console.log("click del button");
+            this.printedValue = this.inputValue;
+            this.inputValue = '';
         }
     }
 }).mount('#app');
