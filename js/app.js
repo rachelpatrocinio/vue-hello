ff2ec1cd809e0ce660906fc173480305b08db46d
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-03-25 15:21:45 +0100

    Initial Commit: Printed on screen with Vue

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const { createApp } = Vue;
+
+createApp({
+    data(){
+        return {
+            titolo: "Il mio primo esercizio Vue",
+            data: '25/03/2024',
+        }
+    }
+}).mount('#app');
